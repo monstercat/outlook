9c2d5b8ce68e467ba82c81a37a75405b03a1f35f
Zhangyangxu <zhangyangxu@gmail.com>
2013-11-18 10:18:34 -0800

    added form animation

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 console.log('loade js file');
 
 $(document).ready(function () {
+  var sideBar_open = false;
+
   $('.gift-form').submit(function (e) {
       console.log('submit form');    
-      e.preventDefault();
+      e.preventDefault()
       var data = { test:"test" };
+      console.log('show submit');
+      console.log(data);
+
       $.ajax({
             type: 'POST', 
-            data: data,
             url: 'http://localhost:8000/gift',
+            contentType: "application/json; charset=utf-8",
+            dataType: 'json',
+            data: JSON.stringify(data),
             success: function(data) {
               alert('success');
             }, error: function (data) {
-              alert('failed');
+              return;
             }
       });
   });
 
-  $(".friend-email").click(function(){
-    console.log('show click');
-    $(".side-bar").css("right","-500px");
+  $(".gift").click(function(){
+    if(sideBar_open){
+      $(".side-bar").animate({right:"-400px"})
+    }else{
+      $(".side-bar").animate({right:"0px"})
+    }
+    sideBar_open = !sideBar_open;
   });
 });
 
